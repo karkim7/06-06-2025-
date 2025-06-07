47dc46f7ca66ec9952b674d242ceb4b630ce117b
Mahesh Karki <karkim@carolinau.edu>
2025-06-07 04:58:41 +0000

    Practice 2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Person
+{
+    public string name;
+
+    //constructor
+    public Person(string personName)
+    {
+        name = personName;
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello, " + name);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creatong Object from class
+        Person p1 = new Person("Mahesh");
+        Person p2 = new Person("Sita");
+
+        //calling method
+
+        p1.SayHello();
+        p2.SayHello();
+          }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 06-06-2025-
 Concept of SIMPLE constructors in C#
+
+Practice Day 02
+ 06 06 2025
